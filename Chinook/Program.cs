f5e062a7f3169e173a40324f85589dfe46fe854a
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-31 19:52:34 +0200

    Created GetAllCustomers in CustomerRepository

--- Chinook/Program.cs
+++ Chinook/Program.cs
@@ -1,2 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Chinook.Models;
+using Chinook.Repositories;
+
 Console.WriteLine("Hello, World!");
+
+static void ReadAllCustomers(ICustomerRepository repository)
+{ 
+    ReadCustomers(repository.GetAllCustomers());
+}
+
+static void ReadOneCustomer(ICustomerRepository repository) 
+{
+    ReadCustomer(repository.GetCustomer("2"));
+}
+
+static void AddRecord (ICustomerRepository repository) 
+{
+    Customer customer = new Customer();
+}
+
+static void UpdateRecord (ICustomerRepository repository) 
+{
+    Customer customer = new Customer();
+}
+
+
+//Methods for the ReadCustomer methods above
+static void ReadCustomers(IEnumerable<Customer> customers) //reads several customers. uses the ReadCustomer method below for reading/printing customers
+{
+    foreach (Customer customer in customers)
+    {
+        ReadCustomer(customer);
+    }
+}
+
+static void ReadCustomer(Customer customer) //Reads one customer
+{
+    Console.WriteLine($"{customer.CustomerId}, {customer.FirstName}, {customer.LastName}, {customer.Country}, {customer.PostalCode}, {customer.Phone}, {customer.Email}");
+}
---
+++ Chinook/Repositories/CustomerRepository.cs
@@ -0,0 +1,85 @@
+﻿using Chinook.Models;
+using Chinook.Repository;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Repositories
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public List<Customer> GetAllCustomers(int limit, int offset)
+        {
+            List<Customer> customerList = new List<Customer>();
+            //Write the SQL statement to get all customers
+            string sql = "SELECT CustomerID, FirstName, LastName, Country, PostalCode, Phone, Email FROM CUSTOMER";
+            try
+            {
+                //write the statement for connection (to connect)
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionHelper.GetConnectionString())) 
+                {
+                    sqlConnection.Open(); //Open the connection
+                    //Write the command for the sql
+                    using (SqlCommand command = new SqlCommand(sql, sqlConnection)) 
+                    {
+                        //Write the statement to execute query
+                        //SQLDataReader
+                        using (SqlDataReader sqlDataReader = command.ExecuteReader()) 
+                        {
+                            while (sqlDataReader.Read()) 
+                            {
+                                //Create a customer object
+                                Customer tempCustomer = new Customer();
+                                //Assign value from database to the ID
+                                tempCustomer.CustomerId = sqlDataReader.GetInt32(0);
+                                tempCustomer.FirstName = sqlDataReader.GetString(1);
+                                tempCustomer.LastName = sqlDataReader.GetString(2);
+                                tempCustomer.Country = sqlDataReader.GetString(3);
+                                tempCustomer.PostalCode = sqlDataReader.GetString(4);
+                                tempCustomer.Phone = sqlDataReader.GetString(5);
+                                tempCustomer.Email = sqlDataReader.GetString(6);
+                                //Add to the collection list
+                                customerList.Add(tempCustomer);
+
+
+                            }
+                        }
+                    } 
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message, ex);
+            }
+            return customerList;
+        }
+
+        public Customer GetCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetCustomer(string name)
+        {
+            throw new NotImplementedException();
+        }
+        public bool AddNewCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
