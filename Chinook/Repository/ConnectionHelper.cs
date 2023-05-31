e66ffbb7c509830efa2978d63b9afa389e0049e9
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-31 17:55:01 +0200

    Created method GetConnectionString
    Method in class ConnectionHelper for the connection string (for the connection between console application and database)

---
+++ Chinook/Repository/ConnectionHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Repository
+{
+    public class ConnectionHelper
+    {
+        //method for the connection string (for the connection between console application and database)
+
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder();
+            sqlConnectionStringBuilder.DataSource = "BETTY\\SQLEXPRESS";
+            sqlConnectionStringBuilder.InitialCatalog = "Chinook";
+            sqlConnectionStringBuilder.IntegratedSecurity = true;
+            return sqlConnectionStringBuilder.ConnectionString;
+        }
+    }
+}
