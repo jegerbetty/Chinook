d13fae6fddd3c992a5eeec90c4df8b6f23a7eb7d
jegerbetty <115633564+jegerbetty@users.noreply.github.com>
2023-05-31 20:11:22 +0200

    Created methods for getcustomer by id and by name

--- Chinook/Repositories/CustomerRepository.cs
+++ Chinook/Repositories/CustomerRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,11 +13,12 @@ namespace Chinook.Repositories
 {
     public class CustomerRepository : ICustomerRepository
     {
-        public List<Customer> GetAllCustomers(int limit, int offset)
+        public List<Customer> GetAllCustomers(int limit, int offset) //Read all the customers in the database, this should display their: Id, first name, last name, country, postal code, phone number and email.
         {
             List<Customer> customerList = new List<Customer>();
             //Write the SQL statement to get all customers
-            string sql = "SELECT CustomerID, FirstName, LastName, Country, PostalCode, Phone, Email FROM CUSTOMER";
+            string sql = "SELECT CustomerID, FirstName, LastName, Country, PostalCode, Phone, Email" + 
+                         "FROM CUSTOMER";
             try
             {
                 //write the statement for connection (to connect)
@@ -58,14 +60,89 @@ namespace Chinook.Repositories
             return customerList;
         }
 
-        public Customer GetCustomer(int id)
+        public Customer GetCustomerByID(int id) //Read a specific customer from the database (by Id), should display their: Id, first name, last name, country, postal code, phone number and email.
         {
-            throw new NotImplementedException();
+            Customer customer = new Customer();
+            
+            string sql = "SELECT CustomerID, FirstName, LastName, Country, PostalCode, Phone, Email" + 
+                         "FROM CUSTOMER" + 
+                         "WHERE CustomerID=@CustomerID";
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionHelper.GetConnectionString()))
+                {
+                    sqlConnection.Open(); 
+                    
+                    using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@CustomerID", customer.CustomerId); //provide PK value for 1 customer
+                        using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+                                
+                                Customer tempCustomer = new Customer();
+                                
+                                tempCustomer.CustomerId = sqlDataReader.GetInt32(0);
+                                tempCustomer.FirstName = sqlDataReader.GetString(1);
+                                tempCustomer.LastName = sqlDataReader.GetString(2);
+                                tempCustomer.Country = sqlDataReader.GetString(3);
+                                tempCustomer.PostalCode = sqlDataReader.GetString(4);
+                                tempCustomer.Phone = sqlDataReader.GetString(5);
+                                tempCustomer.Email = sqlDataReader.GetString(6);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex);
+            }
+            return customer;
         }
 
-        public Customer GetCustomer(string name)
+        public Customer GetCustomerByName(string partialname) //Read a specific customer from the database (by name). HINT: LIKE keyword can help for partial matches. Should display their: Id, first name, last name, country, postal code, phone number and email.
         {
-            throw new NotImplementedException();
+            Customer customer = new Customer();
+
+            string sql = "SELECT CustomerID, FirstName, LastName, Country, PostalCode, Phone, Email" + 
+                         "FROM CUSTOMER" + 
+                         "WHERE FirstName LIKE @partialname" +
+                         "OR LastName LIKE @partialname";
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConnectionHelper.GetConnectionString()))
+                {
+                    sqlConnection.Open();
+
+                    using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+                    {
+                        command.Parameters.AddWithValue("@partialname", partialname); //provide partial name value for customer
+                        using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                        {
+                            while (sqlDataReader.Read())
+                            {
+
+                                Customer tempCustomer = new Customer();
+
+                                tempCustomer.CustomerId = sqlDataReader.GetInt32(0);
+                                tempCustomer.FirstName = sqlDataReader.GetString(1);
+                                tempCustomer.LastName = sqlDataReader.GetString(2);
+                                tempCustomer.Country = sqlDataReader.GetString(3);
+                                tempCustomer.PostalCode = sqlDataReader.GetString(4);
+                                tempCustomer.Phone = sqlDataReader.GetString(5);
+                                tempCustomer.Email = sqlDataReader.GetString(6);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message, ex);
+            }
+            return customer;
         }
         public bool AddNewCustomer(Customer customer)
         {
--- Chinook/Repositories/ICustomerRepository.cs
+++ Chinook/Repositories/ICustomerRepository.cs
@@ -10,8 +10,8 @@ namespace Chinook.Repositories
     public interface ICustomerRepository
     {
         public List<Customer> GetAllCustomers(); //returns all customers. display: Id, firstname, lastname, country, postal cost, phone number, email
-        public Customer GetCustomer(int id); //Returns one customer by Id. get one customer by Id. display: Id, firstname, lastname, country, postal cost, phone number, email
-        public Customer GetCustomer(string name); //get one customer by name (HINT: LIKE keyword can help for partial matches). display: Id, firstname, lastname, country, postal cost, phone number, email
+        public Customer GetCustomerByID(int id); //Returns one customer by Id. get one customer by Id. display: Id, firstname, lastname, country, postal cost, phone number, email
+        public Customer GetCustomerByName(string partialname); //get one customer by name (HINT: LIKE keyword can help for partial matches). display: Id, firstname, lastname, country, postal cost, phone number, email
         public List<Customer> GetAllCustomers(int limit, int offset); //return a page of customers from the database. This should take in limit and offset as parameters. display: Id, firstname, lastname, country, postal cost, phone number, email
         public bool AddNewCustomer(Customer customer); //add a new customer to the database. add: Id, firstname, lastname, country, postal cost, phone number, email
         public bool UpdateCustomer(Customer customer); //update an existing customer
